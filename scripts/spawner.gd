2510cd97d29dd5c2caa0518ce82e6c7cc485c3a6
lolyay270 <boyeje2@student.op.ac.nz>
2025-07-29 10:14:03 +1200

    fix(spawner): not every frame

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	print("runTime: ", gameManager.runTime, "   lastSpawn: ", lastSpawn, "   delay: ", delay)
-	if (lastSpawn + delay >= gameManager.runTime):
+	if (lastSpawn + delay <= gameManager.runTime):
 		
 		#spawn object
 		var instance: Node2D = fish1.instantiate()
