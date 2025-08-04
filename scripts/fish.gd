76d5630a0b8d092e4ecc9cb2ec2affd5e3de6d09
lolyay270 <boyeje2@student.op.ac.nz>
2025-08-05 10:23:52 +1200

    feat(fish): jump on screen

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -3,16 +3,25 @@ extends RigidBody2D
 @export var spawnLeft: bool # fish spawns on left half to jump to the right
 @export var horDistance: float
 @export var vertDistance: float
+@export var ySpawnPos: float
 
-func _ready() -> void:
-	if (!spawnLeft):
-		horDistance *= -1
-	apply_central_impulse(Vector2(horDistance,vertDistance * -1))
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _process(delta: float) -> void:
-	pass
-	# rotate based on screen position - set_angular_velicity(speed: float)
-	# 
+	if global_position.y > ySpawnPos + 100:
+		self.queue_free()
+
+# only start moving the fish when spawner gives values
+func Start():
+	if (!spawnLeft):
+		sprite.flip_v = true
+	
+	gravity_scale = 1 # start gravity 
+	
+	if (!spawnLeft):
+		horDistance *= -1
+	
+	apply_central_impulse(Vector2(horDistance / 2.8, vertDistance * -2))
 
 func _physics_process(delta: float) -> void:
 	look_at(linear_velocity + position)
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -9,8 +9,8 @@ const minTime: float = .1
 const maxTime: float = 1
 
 const sideRatio: float = 0.5 # 50% left side spawns, 50% right
-const spawnZonePadding: float = 0.05 # percent of screen around each spawn zone that cannot be spawned in
-const ySpawnPos: float = 0 # spawn location of fish below the screen
+const spawnZonePadding: float = 0.1 # percent of screen around each spawn zone that cannot be spawned in
+var ySpawnPos: float # spawn location of fish below the screen
 
 var delay: float;
 var lastSpawn: float = 0
@@ -19,38 +19,54 @@ var lastSpawn: float = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SetRandomDelay()
+	ySpawnPos = viewportSize.y + 250
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	viewportSize = Vector2(get_viewport().size)
-	#print("runTime: ", gameManager.runTime, "   lastSpawn: ", lastSpawn, "   delay: ", delay)
+
 	if (lastSpawn + delay <= gameManager.runTime):
+		
 		#spawn object
-		var instance = fish1.instantiate()
-		add_child(instance)
+		var fish = fish1.instantiate()
+		add_child(fish)
 		
-		#left half or right spawn?
-		var side = randf()
-		var posMin
-		var posMax
+		GiveFishRandomValues(fish)
 		
-		if (side < sideRatio):
-			instance.spawnLeft = true;
-			posMin = viewportSize * spawnZonePadding
-			posMax = (viewportSize / 2) - posMin
-		else:
-			instance.spawnLeft = false;
-			# posMin = 
-			# posMax = 
-		
-		var xPos = randf_range(posMin, posMax)
-		instance.transform.origin = Vector2(xPos, ySpawnPos)
+		fish.Start()
 		
 		# reset timer vars
 		lastSpawn = gameManager.runTime
 		SetRandomDelay()
 
+
+func GiveFishRandomValues(fish):
+	#left half or right spawn?
+	var side = randf()
+	var posMin
+	var posMax
+	
+	# spawn position (within the left/right halfs of screen) randomisation
+	if (side < sideRatio):
+		fish.spawnLeft = true
+		posMin = viewportSize.x * spawnZonePadding
+		posMax = (viewportSize.x / 2) - spawnZonePadding
+	else:
+		fish.spawnLeft = false
+		posMin = (viewportSize.x / 2) + spawnZonePadding
+		posMax = viewportSize.x - (viewportSize.x * spawnZonePadding) 
+	
+	var xPos = randf_range(posMin, posMax)
+	fish.transform.origin = Vector2(xPos, ySpawnPos)
+	
+	if (fish.spawnLeft):
+		fish.horDistance = viewportSize.x - xPos - spawnZonePadding
+	else:
+		fish.horDistance = xPos - spawnZonePadding
+	
+	fish.vertDistance = viewportSize.y - spawnZonePadding
+	fish.ySpawnPos = ySpawnPos
 
 func SetRandomDelay() -> void:
 	delay = randf_range(minTime, maxTime)
