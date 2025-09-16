91633eb1eed1b9e43bffd433fb7cf48afceb8d7a
lolyay270 <boyeje2@student.op.ac.nz>
2025-09-16 16:31:31 +1200

    feat(touch): fish only netted on up swipe

--- scripts/net.gd
+++ scripts/net.gd
@@ -4,23 +4,28 @@ This script manages the trigger area and visuals of the net object
 
 extends Sprite2D
 
+#external scripts
 @onready var healthManager = $"/root/Game/Health"
 @onready var scoreManager = $"/root/Game/Score"
 
+# related to net
 @onready var trigger = $Area2D/CollisionPolygon2D
+@onready var touch = $".."
 
 
 func _on_area_entered(area: Area2D) -> void:
-	#get pathFollow, then path, then object's parent node
-	var object: Node2D = area.get_parent().get_parent().get_parent()  
-	var objType: GameManager.ObjectTypes = object.type
-	
-	if objType == GameManager.ObjectTypes.Bomb:
-		healthManager.UpdateHealth(-1)
-	else:
-		scoreManager.UpdateScore(1)
-	
-	object.queue_free()
+	#only check and destroy obj if net moving upwards
+	if touch.canCatch: 
+		#get pathFollow, then path, then object's parent node
+		var object: Node2D = area.get_parent().get_parent().get_parent()  
+		var objType: GameManager.ObjectTypes = object.type
+		
+		if objType == GameManager.ObjectTypes.Bomb:
+			healthManager.UpdateHealth(-1)
+		else:
+			scoreManager.UpdateScore(1)
+		
+		object.queue_free()
 
 
 func ChangeStatus(value: bool):
--- scripts/playerTouch.gd
+++ scripts/playerTouch.gd
@@ -1,8 +1,19 @@
+"""
+This script manages the player's touch during game play, if it is active, and if it is swiping upwards
+"""
+
 extends Node2D
 
 @onready var net = $net
 
+#how many of the last frames to calc if the touch is moving up
+const FRAMES_TO_SAVE: int = 5
+
 var pressed: bool = false
+var lastFramesYPos: Array[float] = [] #holds the last few frames Y pos, FIFO system
+
+#determines if the mouse has been moving up (lower in y values) the last few frames
+@export var canCatch: bool = false 
 
 func _ready() -> void:
 	net.hide()
@@ -12,6 +23,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if pressed:
 		self.global_position = get_global_mouse_position()
+		CheckUpSwipe()
 
 
 func _input(event: InputEvent) -> void:
@@ -23,6 +35,24 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_released("press"):
 		net.ChangeStatus(false)
 		pressed = false
+
+
+func CheckUpSwipe():
+	#save the most recent y pos, FIFO system
+	lastFramesYPos.append(get_global_mouse_position().y)
+	if lastFramesYPos.size() > FRAMES_TO_SAVE:
+		lastFramesYPos.remove_at(0)
+	
+	var upwardCount = 0
+	
+	for i in lastFramesYPos.size() - 1:
+		if lastFramesYPos[i] > lastFramesYPos[i + 1]: #moving upwards on screen
+			upwardCount += 1
+		else:
+			canCatch = false
+	
+	if upwardCount == FRAMES_TO_SAVE - 1:
+		canCatch = true
 
 
 func HandlePlayStateUpdate():
