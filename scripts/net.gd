3403613e55d6a4ff509efe5ae3a967b0cddd869d
lolyay270 <boyeje2@student.op.ac.nz>
2025-11-17 11:33:37 +1300

    fix(save): when discovering obj

--- scripts/net.gd
+++ scripts/net.gd
@@ -27,6 +27,8 @@ func _on_area_entered(area: Area2D) -> void:
 		# save that the obj has been collected
 		if GameManager.discovered.get(object.stats.name) == false:
 			GameManager.discovered.set(object.stats.name, true)
+			
+			SaveData.save_game()
 		
 		object.queue_free()
 
